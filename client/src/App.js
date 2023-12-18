fe358d13d1187f0466e0845104279462935f3e03
tanluc1e <t.luc497303@gmail.com>
2023-12-18 21:08:36 +0700

    setup (client): Store, Reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Store from 'stores/Store';
 import Home from 'routes/Home';
+import Login from 'routes/Auth/Login';
+import Register from 'routes/Auth/Register';
 import Layout from 'components/Layout';
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Store>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </Store>
   );
 }
 
