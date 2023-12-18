42160f7b63ceec24e05a64469753744fa3c6364e
tanluc1e <t.luc497303@gmail.com>
2023-12-18 20:53:51 +0700

    feat (client): New sidebar, clean code

--- client/src/components/Layout/Sidebar/index.jsx
+++ client/src/components/Layout/Sidebar/index.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import './style.css'
 
-// import icons
-import { GoHome } from "react-icons/go";
-import { FaRegUser } from "react-icons/fa6";
-import { PiNewspaper } from "react-icons/pi";
-import { MdOutlineFolder, MdOutlineAccountCircle } from "react-icons/md";
+// import icons 
+import { GoHome, GoHomeFill } from "react-icons/go";
+import { MdOutlineDashboard, MdDashboard } from "react-icons/md";
+import { RiGroupLine, RiGroupFill } from "react-icons/ri";
+import { IoLayersOutline, IoLayers } from "react-icons/io5";
+import { FaRegUserCircle, FaUserCircle } from "react-icons/fa";
 import { BsQuestionCircle } from "react-icons/bs";
 
 import logo from 'assets/logo.png'
 
 export default function Sidebar() {
+    const location = useLocation();
+
+    const menuItems = [
+        { title: 'Home', path: '/', icon: GoHome, selectIcon: GoHomeFill },
+        { title: 'All boards', path: '/boards', icon: MdOutlineDashboard, selectIcon: MdDashboard },
+        { title: 'Resources', path: '/files', icon: IoLayersOutline, selectIcon: IoLayers },
+        { title: 'User', path: '/user', icon: RiGroupLine, selectIcon: RiGroupFill },
+    ];
+
+    const settingItems = [
+        { title: 'Accounts', path: '/accounts', icon: FaRegUserCircle, selectIcon: FaUserCircle }
+    ];
     return (
         <div className='sideBar grid'>
-            <div className='logoDiv flex'>
+            <Link to={"/"} className='logoDiv flex'>
                 <img src={logo} alt="Logo" />
                 <h2>4rums</h2>
-            </div>
+            </Link>
 
             <div className='menuDiv'>
                 <h3 className='divTitle'>
@@ -24,54 +38,20 @@ export default function Sidebar() {
                 </h3>
 
                 <ul className='menuLists grid'>
-
-                    <li className='listItem'>
-                        <a href="#!" className='menuLink flex'>
-                            <GoHome className='icon' />
-                            <span className='smallText'>
-                                Home
-                            </span>
-                        </a>
-                    </li>
-
-                    <li className='listItem'>
-                        <a href="#!" className='menuLink flex'>
-                            <GoHome className='icon' />
-                            <span className='smallText'>
-                                All Boards
-                            </span>
-                        </a>
-                    </li>
-
-                    <li className='listItem'>
-                        <a href="#!" className='menuLink flex'>
-                            <MdOutlineFolder className='icon' />
-                            <span className='smallText'>
-                                File/Uploads
-                            </span>
-                        </a>
-                    </li>
-
-                    <li className='listItem'>
-                        <a href="#!" className='menuLink flex'>
-                            <FaRegUser className='icon' />
-                            <span className='smallText'>
-                                Users
-                            </span>
-                        </a>
-                    </li>
-
-                    <li className='listItem'>
-                        <a href="#!" className='menuLink flex'>
-                            <PiNewspaper className='icon' />
-                            <span className='smallText'>
-                                Rules
-                            </span>
-                        </a>
-                    </li>
-
+                    {menuItems.map((menu, index) => (
+                        <li className={`listItem ${location.pathname === menu.path && 'active'}`}>
+                            <Link to={menu.path} className='menuLink flex'>
+                                {location.pathname === menu.path ? <menu.selectIcon className='icon' /> : <menu.icon className='icon' />}
+                                <span className='smallText'>
+                                    {menu.title}
+                                </span>
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </div>
+
+            <div className="sidebar-line"></div>
 
             <div className='settingsDiv'>
                 <h3 className='divTitle'>
@@ -79,16 +59,16 @@ export default function Sidebar() {
                 </h3>
 
                 <ul className='menuLists grid'>
-
-                    <li className='listItem'>
-                        <a href="#!" className='menuLink flex'>
-                            <MdOutlineAccountCircle className='icon' />
-                            <span className='smallText'>
-                                Accounts
-                            </span>
-                        </a>
-                    </li>
-
+                    {settingItems.map((menu, index) => (
+                        <li className={`listItem ${location.pathname === menu.path && 'active'}`}>
+                            <Link to={menu.path} className='menuLink flex'>
+                                {location.pathname === menu.path ? <menu.selectIcon className='icon' /> : <menu.icon className='icon' />}
+                                <span className='smallText'>
+                                    {menu.title}
+                                </span>
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </div>
 
@@ -99,7 +79,7 @@ export default function Sidebar() {
                     <div className="circle2"></div>
 
                     <h3>Help Center</h3>
-                    <p>Having trouble in Plant, please contact us from for more questions.</p>
+                    <p>Having trouble in 4rums, please contact us from for more questions.</p>
                     <button className='btn'>Go to help center</button>
                 </div>
             </div>
