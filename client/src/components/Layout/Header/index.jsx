26d9b163c1533d6bd645febf5b1aa5f91383dd50
tanluc1e <t.luc497303@gmail.com>
2023-12-18 20:18:27 +0700

    test (client): not logged in and logged in header

--- client/src/components/Layout/Header/index.jsx
+++ client/src/components/Layout/Header/index.jsx
@@ -4,14 +4,22 @@ import './style.css'
 /* import icons */
 import { BiSearchAlt } from "react-icons/bi";
 import { TbMessageCircle } from "react-icons/tb";
-import { MdOutlineNotificationsNone } from "react-icons/md";
+import { IoNotifications } from "react-icons/io5";
+
+import { BsArrowRightShort } from "react-icons/bs";
+import { TiUserAdd } from "react-icons/ti";
+import { MdOutlineLogin } from "react-icons/md";
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <div className='topSection'>
             <div className='headerSection flex'>
-                <div className="title">
-                    <h1>Welcome to Plant</h1>
+
+                {/* ------------------------ LOGGED IN ------------------------ */}
+
+                {/* <div className="title">
+                    <h1>Welcome to 4rums</h1>
                     <p>Hello TanLuc, Welcome back!</p>
                 </div>
 
@@ -22,10 +30,31 @@ export default function Header() {
 
                 <div className="adminDiv flex">
                     <TbMessageCircle className='icon' />
-                    <MdOutlineNotificationsNone className='icon' />
+                    <IoNotifications className='icon' />
                     <div className='adminImage'>
                         <img src="https://api.dicebear.com/7.x/notionists-neutral/svg?seed=Rascal" alt="Avatar" />
                     </div>
+                </div> */}
+
+
+
+                {/* ------------------------ NOT LOGGED IN ------------------------ */}
+
+                <div className="title">
+                    <h1>Welcome to 4rums</h1>
+                    <p>Login to use many features!</p>
+                </div>
+
+                <div className="buttonDiv flex">
+                    <Link to={"/login"} className='loginBtn btn flex'>
+                        <span>Login</span>
+                        <MdOutlineLogin className='icon' />
+                    </Link>
+
+                    <Link to={"/register"} className='signUpBtn btn flex'>
+                        <span>Sign Up</span>
+                        <TiUserAdd className='icon' />
+                    </Link>
                 </div>
             </div>
         </div>
